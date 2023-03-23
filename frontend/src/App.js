5211ceb1d18de407eb0e4974e27892d4f479bf55
joannaberlin <joanna.kot.ok@gmail.com>
2023-03-23 13:37:23 +0100

    Add Newsletter and defer to EventDetail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import EditEvent from './pages/EditEvent';
-import EventDetail, {
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
+import EventDetailPage, {
 	loader as eventDetailLoader,
 	action as deleteEventAction,
 } from './pages/EventDetail';
-import Events, { loader as eventsLoader } from './pages/Events';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRootLayout';
-import Home from './pages/Home';
-import NewEvent from './pages/NewEvent';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/RootLayout';
-import Error from './pages/Error';
 import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <RootLayout />,
-		errorElement: <Error />,
+		errorElement: <ErrorPage />,
 		children: [
-			{ index: true, element: <Home /> },
+			{ index: true, element: <HomePage /> },
 			{
 				path: 'events',
 				element: <EventsRootLayout />,
 				children: [
 					{
 						index: true,
-						element: <Events />,
+						element: <EventsPage />,
 						loader: eventsLoader,
 					},
 					{
@@ -35,18 +37,27 @@ const router = createBrowserRouter([
 						children: [
 							{
 								index: true,
-								element: <EventDetail />,
+								element: <EventDetailPage />,
 								action: deleteEventAction,
 							},
 							{
 								path: 'edit',
-								element: <EditEvent />,
+								element: <EditEventPage />,
 								action: manipulateEventAction,
 							},
 						],
 					},
-					{ path: 'new', element: <NewEvent />, action: manipulateEventAction },
+					{
+						path: 'new',
+						element: <NewEventPage />,
+						action: manipulateEventAction,
+					},
 				],
+			},
+			{
+				path: 'newsletter',
+				element: <NewsletterPage />,
+				action: newsletterAction,
 			},
 		],
 	},
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,22 +1,23 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { json, useLoaderData, defer, Await } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 const Events = () => {
-	const data = useLoaderData();
+	const { events } = useLoaderData();
 
-	if (data.isError) {
-		return <p>{data.message}</p>;
-	}
-
-	const events = data.events;
-
-	return <EventsList events={events} />;
+	return (
+		<Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+			<Await resolve={events}>
+				{(loadedEvents) => <EventsList events={loadedEvents} />}
+			</Await>
+		</Suspense>
+	);
 };
 
 export default Events;
 
-export const loader = async () => {
+const loadEvents = async () => {
 	const response = await fetch('http://localhost:8080/events');
 
 	if (!response.ok) {
@@ -31,6 +32,13 @@ export const loader = async () => {
 			}
 		);
 	} else {
-		return response;
+		const resData = await response.json();
+		return resData.events;
 	}
+};
+
+export const loader = () => {
+	return defer({
+		events: loadEvents(),
+	});
 };
